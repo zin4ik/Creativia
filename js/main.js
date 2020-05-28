eb83ca2b5a729936c65b94151d7af3644697175a
zin4ik <43877261+zin4ik@users.noreply.github.com>
2020-05-28 20:35:10 +0300

    popravki

--- js/main.js
+++ js/main.js
@@ -1,4 +1,80 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+  //nav menu
+  // Cache selectors
+  var lastId,
+    topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function () {
+      var item = $($(this).attr("href"));
+      if (item.length) {
+        return item;
+      }
+    });
+
+  // Bind click handler to menu items
+  // so we can get a fancy scroll animation
+  menuItems.click(function (e) {
+    var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $("html, body").stop().animate(
+      {
+        scrollTop: offsetTop,
+      },
+      1000
+    );
+    e.preventDefault();
+  });
+
+  // Bind to scroll
+  $(window).scroll(function () {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function () {
+      if ($(this).offset().top < fromTop) return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+      lastId = id;
+      // Set/remove active class
+      menuItems
+        .addClass("text-white-50")
+        .parent()
+        .removeClass("active-menu")
+        .end()
+        .filter("[href='#" + id + "' ]")
+        .removeClass("text-white-50")
+        .parent()
+        .addClass("active-menu");
+    }
+  });
+
+  //click collapse
+
+  $(".accordion__btn").on("click", function () {
+    var btn = $(this);
+    var id = btn.attr("data-target");
+    console.log($(id));
+    $(id).on("hidden.bs.collapse", function () {
+      console.log("click3");
+      btn.children(".fa-minus").css("opacity", "0");
+      btn.children(".fa-plus").css("opacity", "1");
+    });
+    $(id).on("shown.bs.collapse", function () {
+      console.log("click2");
+      btn.children(".fa-minus").css("opacity", "1");
+      btn.children(".fa-plus").css("opacity", "0");
+    });
+  });
+
+  //slick slider
   $(".variable-width").slick({
     dots: false,
     arrows: false,
@@ -15,8 +91,8 @@ jQuery(document).ready(function($) {
           arrows: false,
           centerMode: true,
           centerPadding: "40px",
-          slidesToShow: 2
-        }
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 480,
@@ -24,10 +100,10 @@ jQuery(document).ready(function($) {
           arrows: false,
           centerMode: true,
           centerPadding: "40px",
-          slidesToShow: 1
-        }
-      }
-    ]
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
   $(".blog__card").slick({
     slidesToShow: 2,
@@ -35,13 +111,13 @@ jQuery(document).ready(function($) {
     arrows: true,
     vertical: true,
     autoplay: false,
-    prevArrow: $('.card-prev'),
-    nextArrow: $('.card-next'),
-    verticalSwiping: true
+    prevArrow: $(".card-prev"),
+    nextArrow: $(".card-next"),
+    verticalSwiping: true,
   });
   //text-overflow
   function cropTitle() {
-    $(".blog__card-block_descr").each(function() {
+    $(".blog__card-block_descr").each(function () {
       var $title = $(this).find("p");
       while ($title.height() > $(this).height()) {
         $title.text(
@@ -57,4 +133,14 @@ jQuery(document).ready(function($) {
   cropTitle();
 
   //--/text-overflow
+
+  //send masege
+  $(".form-block_title").on("click", function () {
+    var formBlock = $(".form-block");
+    if (formBlock.hasClass("form-block-active")) {
+      formBlock.removeClass("form-block-active");
+    } else {
+      formBlock.addClass("form-block-active");
+    }
+  });
 });
